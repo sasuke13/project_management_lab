70c92fc2100c156cb0373741774452a4ddc27750
Vladislav Kiy <v.kyy@nltu.lviv.ua>
2023-11-29 15:40:20 +0200

    feature: added names for urls, features: added create superuser logic, refactoring: make blank=True for phone_number

--- src/accounts/serializers.py
+++ src/accounts/serializers.py
@@ -19,3 +19,22 @@ class UserSerializer(serializers.ModelSerializer):
         instance = User.objects.create_user(**validated_data)
         instance.save()
         return instance
+
+
+class SuperUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['name', 'surname', 'email', 'password', 'phone_number']
+
+        extra_kwargs = {
+            'name': {'required': True},
+            'surname': {'required': True},
+            'email': {'required': True},
+            'password': {'write_only': True, 'required': True},
+            'phone_number': {'required': True},
+        }
+
+    def create(self, validated_data):
+        instance = User.objects.create_superuser(**validated_data)
+        instance.save()
+        return instance
--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserView, LogoutView
+from .views import RegisterView, LoginView, UserView, LogoutView, CreateSuperuserView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('user/', UserView.as_view()),
-    path('logout/', LogoutView.as_view()),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('user/', UserView.as_view(), name='user'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('create_superuser/', CreateSuperuserView.as_view(), name='create-superuser')
 ]
