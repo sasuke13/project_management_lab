b1f8e267fd69f05c2f2fd6ff5aa9b3af295dc9fd
bogd1 <bgnydyn@gmail.com>
2023-11-29 13:13:09 +0200

    feature: create order by authenticated user

--- src/config/urls.py
+++ src/config/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+api_urls = [
+    path('authorization/', include('accounts.urls')),
+    path('orders/', include('orders.urls')),
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),
-    path('orders/', include('orders.urls')),
+    path('api/v1/', include(api_urls))
 ]
--- src/orders/views.py
+++ src/orders/views.py
@@ -1,10 +1,35 @@
-from rest_framework import generics
+from django.db import transaction
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-from orders.models import Orders
-from orders.serializers import OrdersModelSerializer
+from accounts.models import User
+from accounts.views import verify_user
+from orders.models import Orders, States
+from orders.serializers import OrdersSerializer
 
 
 # Create your views here.
 class OrdersListView(generics.ListAPIView):
     queryset = Orders.objects.all()
-    serializer_class = OrdersModelSerializer
+    serializer_class = OrdersSerializer
+
+
+class CreateOrderView(generics.CreateAPIView):
+    queryset = Orders.objects.all()
+    serializer_class = OrdersSerializer
+
+    def create(self, request, *args, **kwargs):
+        state = 'NW'
+
+        token = request.COOKIES.get('jwt')
+        payload = verify_user(token)
+        client = User.objects.filter(uuid=payload['uuid']).first()
+
+        with transaction.atomic():
+            order_history_instance = States.objects.create(states=[state])
+
+            order = Orders.objects.create(state=state, client=client, order_history=order_history_instance)
+
+        serializer = self.get_serializer(order)
+        return Response({'message': 'Order was successfully opened', 'order': serializer.data}, status=status.HTTP_201_CREATED)
+
