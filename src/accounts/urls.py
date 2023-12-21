a53f1495c50a5b1e8d90c35cc193897ef7a4896a
Vladislav Kiy <v.kyy@nltu.lviv.ua>
2023-12-21 13:21:35 +0200

    refactoring: authentication logic

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserView, LogoutView, CreateSuperuserView, UpdateUserView
+
+from .views import RegisterView, UserView, LogoutView, CreateSuperuserView, UpdateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
     path('user/', UserView.as_view(), name='user'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create_superuser/', CreateSuperuserView.as_view(), name='create-superuser'),
